cb2c017f8da16ac8e34769cd80d9fe24627a1c19
Kim <howtoad123@gmail.com>
2023-03-14 11:00:18 +0100

    more set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>ubunto</p>
+      <p className="font-secondaryLogoText text-large text-transparent secondLogoStroke">
+        LANDRUP
+      </p>
+      <p className="font-primaryLogoText text-logoLarge text-logoText leading-4 logoStroke">
+        DANS
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primaryHeading: "#EAEAEA",
-      secondaryGrey: "E9E9E9",
-      standardWhite: "#FFFFFF",
-      darkPurple: "#5E2E53",
-      lightPurple: "##E1A1E9",
-      logoPurple: "#E856EB",
-      primaryBlack: "#000000",
+    extend: {
+      fontFamily: {
+        primaryLogoText: ["Racing Sans One", "cursive"],
+        secondaryLogoText: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        small: "18px",
+        medium: "24px",
+        large: "36px",
+        xLarge: "48px",
+        logoLarge: "72px",
+      },
+      colors: {
+        primaryHeading: "#EAEAEA",
+        primaryText: "#000000",
+        secondaryText: "#FFFFFF",
+        tertiaryText: "#E9E9E9",
+        primaryBG: "#5E2E53",
+        secondaryBG: "#E9E9E9",
+        cardBG: "#E1A1E980",
+        searchBG: "#C4C4C430",
+        logoText: "#E856EB",
+        primaryLogoStroke: "#000000",
+        secondaryLogoStroke: "#431567",
+        logoBar: "#913693",
+        placeholderText: "#999999",
+      },
     },
   },
   plugins: [],
