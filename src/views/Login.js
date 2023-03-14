ec4d063e5707bbd8496fee71aad2fab874e2a789
Kim <howtoad123@gmail.com>
2023-03-14 15:45:04 +0100

    set up of basic login page

--- src/views/Login.js
+++ src/views/Login.js
@@ -24,18 +24,40 @@ const Login = () => {
   }
 
   return (
-    <>
-      <h1 className="text-xLarge">Login</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          <input type="text" placeholder="Brugernavn" name="username" />
-        </label>
-        <label>
-          <input type="password" placeholder="adgangskode" name="password" />
-        </label>
-        <button type="submit">Log ind</button>
-      </form>
-    </>
+    <div
+      className="w-full h-screen"
+      style={{
+        backgroundImage: `url('/images/splash-image.jpg')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="mx-auto max-w-[332px]">
+        <h1 className="text-xLarge text-primaryHeading">Log ind</h1>
+
+        <form onSubmit={handleLogin} className="grid">
+          <label>
+            <input
+              type="text"
+              placeholder="Brugernavn"
+              name="username"
+              className="w-[332px] h-[50px] pl-5 placeholder-placeholderText"
+            />
+          </label>
+          <label>
+            <input
+              type="password"
+              placeholder="adgangskode"
+              name="password"
+              className="w-[332px] h-[50px] pl-5 placeholder-placeholderText mt-4"
+            />
+          </label>
+          <button type="submit" className="buttonStyle mx-auto mt-8">
+            Log ind
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
