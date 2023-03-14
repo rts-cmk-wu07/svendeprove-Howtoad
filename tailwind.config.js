4f50b692bf11308c071776bd7930cf3792b20901
Kim <howtoad123@gmail.com>
2023-03-14 09:30:46 +0100

    more basic set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primaryHeading: "#EAEAEA",
+      secondaryGrey: "E9E9E9",
+      standardWhite: "#FFFFFF",
+      darkPurple: "#5E2E53",
+      lightPurple: "##E1A1E9",
+      logoPurple: "#E856EB",
+      primaryBlack: "#000000",
+    },
   },
   plugins: [],
 };
