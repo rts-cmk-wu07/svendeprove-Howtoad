617f943b256d2e6032a136ef66ff39fa3ebfde1a
Kim <howtoad123@gmail.com>
2023-03-15 14:30:46 +0100

    remove nav if on "/" or "/login"

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navigation from "./Navigation";
 
 export default function Layout() {
+  const location = useLocation();
+
+  const shouldHideNavigation = () => {
+    const hideOnPaths = ["/", "/login"];
+    return hideOnPaths.includes(location.pathname);
+  };
+
   return (
     <>
-      <header>
-        <Navigation />
-      </header>
+      <header>{!shouldHideNavigation() && <Navigation />}</header>
       <main>
         <Outlet />
       </main>
